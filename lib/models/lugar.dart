523e48edd73050123deac118769c01b9eb6d67ab
David Gonsaga <davidgonsaga@gmail.com>
2023-05-16 20:08:14 -0300

    Estrutura de rotas criada

---
+++ lib/models/lugar.dart
@@ -0,0 +1,22 @@
+class Lugar {
+  double longitude;
+  double latitude;
+  String cep;
+  String rua;
+  String cidade;
+  String estado;
+  String descricao;
+  int status;
+
+  Lugar(
+    this.longitude,
+    this.latitude,
+    this.cep,
+    this.rua,
+    this.cidade,
+    this.estado,
+    this.descricao,
+    this.status
+  );
+
+}
---
+++ lib/routes/route_paths.dart
@@ -0,0 +1,5 @@
+class RoutePaths {
+  static const String HOME = '/';
+  static const String CADASTRO = '/cadastro_lugar';
+  static const String LISTALUGAR = '/lista_lugar';
+}
